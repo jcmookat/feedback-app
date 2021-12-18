21650d0996aaec1cee95f4aced0660783d975556
jcmookat <jscardenas717@rocketmail.com>
2021-12-18 10:55:40 +0900

    context

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,7 +1,17 @@
-import { useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 import RatingItem from './RatingItem'
+
 function RatingSelect({ select }) {
 	const [selected, setSelected] = useState(10)
+
+	const { feedbackEdit } = useContext(FeedbackContext)
+
+	useEffect(() => {
+		if (feedbackEdit.edit === true) {
+			setSelected(feedbackEdit.item.rating)
+		}
+	}, [feedbackEdit])
 
 	const handleChange = (e) => {
 		setSelected(+e.currentTarget.value)
